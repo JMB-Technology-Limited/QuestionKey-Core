74e75e53a5468699b1442f576a95f2f344633600
James <james_baster@shelter.org.uk>
2016-03-02 09:45:41 +0000

    Split Admin Controller View and Edit - Node Option

---
+++ src/QuestionKeyBundle/Controller/AdminTreeVersionNodeOptionEditController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace QuestionKeyBundle\Controller;
+
+use Doctrine\ORM\Mapping\Entity;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use QuestionKeyBundle\Form\Type\AdminNodeOptionEditType;
+use QuestionKeyBundle\Form\Type\AdminConfirmDeleteType;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+
+/**
+ *  @license 3-clause BSD
+ *  @link https://github.com/QuestionKey/QuestionKey-Core
+ */
+class AdminTreeVersionNodeOptionEditController extends AdminTreeVersionNodeOptionController
+{
+
+
+
+    public function editAction($treeId, $versionId, $nodeId, $optionId)
+    {
+
+        // build
+        $return = $this->build($treeId, $versionId, $nodeId, $optionId);
+        if (!$this->treeVersionEditable) {
+            throw new AccessDeniedException();
+        }
+
+        //data
+        $form = $this->createForm(new AdminNodeOptionEditType(), $this->nodeOption);
+        $request = $this->getRequest();
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+                $doctrine = $this->getDoctrine()->getManager();
+                $doctrine->persist($this->nodeOption);
+                $doctrine->flush();
+                return $this->redirect($this->generateUrl('questionkey_admin_tree_version_node_option_show', array(
+                    'treeId'=>$this->tree->getId(),
+                    'versionId'=>$this->treeVersion->getId(),
+                    'nodeId'=>$this->node->getId(),
+                    'optionId'=>$this->nodeOption->getId(),
+                )));
+            }
+        }
+
+        return $this->render('QuestionKeyBundle:AdminTreeVersionNodeOptionEdit:edit.html.twig', array(
+            'tree'=>$this->tree,
+            'treeVersion'=>$this->treeVersion,
+            'node'=>$this->node,
+            'nodeOption'=>$this->nodeOption,
+            'form' => $form->createView(),
+        ));
+
+    }
+
+
+    public function deleteAction($treeId, $versionId, $nodeId, $optionId)
+    {
+
+        // build
+        $return = $this->build($treeId, $versionId, $nodeId, $optionId);
+        if (!$this->treeVersionEditable) {
+            throw new AccessDeniedException();
+        }
+
+        //data
+        $form = $this->createForm(new AdminConfirmDeleteType());
+        $request = $this->getRequest();
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+                $doctrine = $this->getDoctrine()->getManager();
+                $doctrine->remove($this->nodeOption);
+                $doctrine->flush();
+                return $this->redirect($this->generateUrl('questionkey_admin_tree_version_node_show', array(
+                    'treeId'=>$this->tree->getId(),
+                    'versionId'=>$this->treeVersion->getId(),
+                    'nodeId'=>$this->node->getId(),
+                )));
+            }
+        }
+
+        return $this->render('QuestionKeyBundle:AdminTreeVersionNodeOptionEdit:delete.html.twig', array(
+            'tree'=>$this->tree,
+            'treeVersion'=>$this->treeVersion,
+            'node'=>$this->node,
+            'nodeOption'=>$this->nodeOption,
+            'form' => $form->createView(),
+        ));
+
+    }
+
+
+
+
+
+}
